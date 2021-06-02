efc55b8bdd67599a03c818a4580a4120c9d2b603
DESKTOP-RDS35E6\최시운 <siun0331@gmail.com>
2021-06-03 01:12:52 +0900

    백준문제풀이/실버4/스택
    후위연산자

---
+++ src/Silver/Main_15815.java
@@ -0,0 +1,49 @@
+package Silver;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_15815 {
+    public static void main(String args[]){
+        Stack<Integer> stack= new Stack<Integer>();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int sum =0;
+        int one,two;
+        for(int i =0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            switch (ch){
+                case'+':
+                    two = stack.pop();
+                    one = stack.pop();
+                    sum = one+two;
+                    stack.push(sum);
+                    break;
+                case'-':
+                    two = stack.pop();
+                    one = stack.pop();
+                    sum = one-two;
+                    stack.push(sum);
+                    break;
+                case'*':
+                    two = stack.pop();
+                    one = stack.pop();
+                    sum = one*two;
+                    stack.push(sum);
+                    break;
+                case'/':
+                    two = stack.pop();
+                    one = stack.pop();
+                    sum = one/two;
+                    stack.push(sum);
+                    break;
+                default:
+                    int n = ch - '0';
+                    stack.push(n);
+                    break;
+            }
+        }
+        System.out.println(stack.peek());
+
+    }
+}

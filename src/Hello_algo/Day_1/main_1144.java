f52056c157ca4cafe69d2991da319d6e50a8d9ef
xilucks <siun0331@gmail.com>
2021-07-21 16:54:43 +0900

    Hello_algo/1144_울타리색칠하기/레이팅 1300

--- src/Hello_algo/Day_1/main_1144.java
+++ src/Hello_algo/Day_1/main_1144.java
@@ -1,10 +1,8 @@
 package Hello_algo.Day_1;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class main_1144 {
     public static void main(String args[]) throws IOException {

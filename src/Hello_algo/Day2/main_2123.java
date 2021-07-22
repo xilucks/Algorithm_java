789fb1d61923935419ffc8a10f47099bc2aa5729
xilucks <siun0331@gmail.com>
2021-07-22 13:32:55 +0900

    Hello_Algo/2123_3장으로 하는 블랙잭/레이팅 1250

---
+++ src/Hello_algo/Day2/main_2123.java
@@ -0,0 +1,29 @@
+package Hello_algo.Day2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class main_2123 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //cardnum
+        int m = sc.nextInt(); //targetnum
+        int [] arr = new int[n];
+        for (int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for (int i =0; i <n-2; i++){
+            for (int j = i+1; j< n-1;j++){
+                for (int k = j+1; k< n; k++){
+                    int total = arr[i]+arr[j]+arr[k];
+                    if (total > max && total<=m){
+                        max = total;
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}

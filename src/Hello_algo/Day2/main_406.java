208d6bb5c73f62be7231f5b6a4909500a9fbf27e
xilucks <siun0331@gmail.com>
2021-07-22 16:33:35 +0900

    Hello_Algo/406_과정 출력하기/레이팅 1100

---
+++ src/Hello_algo/Day2/main_406.java
@@ -0,0 +1,33 @@
+package Hello_algo.Day2;
+
+import java.util.Scanner;
+
+public class main_406 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i<num; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int k = 0; k< num; k++){
+            System.out.print(arr[k]+ " ");
+        }
+        for (int i =num; i>0; i--){
+            for(int j = 0; j<i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = tmp;
+                    System.out.println();
+                    for(int k = 0; k< num; k++){
+                        System.out.print(arr[k]+ " ");
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}

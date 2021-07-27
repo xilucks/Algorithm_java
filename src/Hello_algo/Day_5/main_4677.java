23cfdcf03edfaadf504c7c2548c7c868ca5a7c6e
xilucks <siun0331@gmail.com>
2021-07-27 19:02:26 +0900

    Hello_Algo/4677_서로 다른 수/HashSet,TreeSet

---
+++ src/Hello_algo/Day_5/main_4677.java
@@ -0,0 +1,30 @@
+package Hello_algo.Day_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class main_4677 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        TreeSet<Integer> ts = new TreeSet<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            int x = Integer.parseInt(st.nextToken());
+
+            if (! ts.contains(x)){
+                ts.add(x);
+            }
+            else{
+                continue;
+            }
+        }
+        sb.append(ts.size());
+        System.out.println(sb);
+    }
+}

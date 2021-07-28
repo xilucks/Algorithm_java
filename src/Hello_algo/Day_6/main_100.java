a26dda80d64567c3b58378d52c01ea8734a161d2
xilucks <siun0331@gmail.com>
2021-07-28 20:35:06 +0900

    Hello_Algo/100_배식

---
+++ src/Hello_algo/Day_6/main_100.java
@@ -0,0 +1,27 @@
+package Hello_algo.Day_6;
+
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.ArrayList;
+    import java.util.Collections;
+    import java.util.StringTokenizer;
+
+    public class main_100 {
+        public static void main(String args[]) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            ArrayList<Integer> list = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < n; i++){
+                int x =Integer.parseInt(st.nextToken());
+                list.add(x);
+            }
+            Collections.sort(list);
+            int sum = 0;
+            for(int i = 0; i< n; i++){
+                sum += list.get(i) * (n-i);
+            }
+            System.out.println(sum);
+        }
+    }

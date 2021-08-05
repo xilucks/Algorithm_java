82dd3f37b44532cbbf32ce5b327d7b50d9c90b2f
xilucks <siun0331@gmail.com>
2021-08-05 21:30:53 +0900

    Hello_Algo/4697_포탈/BFS

--- src/Hello_algo/Day_12/main_4697.java
+++ src/Hello_algo/Day_12/main_4697.java
@@ -1,2 +1,41 @@
-package Hello_algo.Day_12;public class main_4697 {
+package Hello_algo.Day_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class main_4697 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int [200001];
+
+        Queue<Integer> queue = new LinkedList<>();
+        dist[X] = 1;
+        queue.offer(X);
+
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+            if(x+1 <= 200000 && dist[x+1] == 0){
+                dist[x+1] = dist[x]+1;
+                queue.offer(x+1);
+            }
+            if(x-1 >=0 && dist[x-1] == 0){
+                dist[x-1] = dist[x]+1;
+                queue.offer(x-1);
+            }
+            if(x*2 <= 200000 && dist[x*2] == 0){
+                dist[x*2] = dist[x]+1;
+                queue.offer(x*2);
+            }
+        }
+
+        System.out.println(dist[Y]-1);
+    }
 }

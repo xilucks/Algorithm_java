f15ce082751634e320a417cc91072ae4b626654c
xilucks <siun0331@gmail.com>
2021-08-26 17:33:04 +0900

    BOJ/1003_피보나치 함수/실버3/DP
    아니 이렇게 푸는거 아닌거 같은데..?

--- src/Baekjoon/Silver/Main_1003.java
+++ src/Baekjoon/Silver/Main_1003.java
@@ -1,45 +1,58 @@
 package Baekjoon.Silver;
 //wrong
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main_1003 {
     static int zerosum = 0;
     static int onesum = 0;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer[]> arr = new ArrayList<>();
+    static Integer[] finbonacci(int n){
+        Integer[] tmp = arr.get(n);
+        int x = tmp[0];
+        int y = tmp[1];
+        if (n==0){
+            return arr.get(n);
+        }
+        else if(n==1){
+            return arr.get(n);
+        }
+        else if (x==0 && y ==0){
+            Integer[] tp1 = finbonacci(n-1);
+            Integer[] tp2 = finbonacci(n-2);
+            int x1 = tp1[0];
+            int x2 = tp2[0];
+            int y1 = tp1[1];
+            int y2 = tp2[1];
+            int xtotal = x1+x2;
+            int ytotal = y1+y2;
+            arr.set(n, new Integer[]{xtotal,ytotal});
+            return arr.get(n);
+        }
+        else{
+            return arr.get(n);
+        }
 
-    static int finbonacci(int n,int []arr){
-        if(n == 0){
-            zerosum++;
-            return arr[0];
-        }
-        else if(n == 1){
-            onesum++;
-            return arr[1];
-        }
-        else if (arr[n] != 0){
-            return arr[n];
-        }
-        else {
-            return arr[n] = finbonacci(n-1,arr)+finbonacci(n-2,arr);
-        }
+
+
     }
     public static void main(String args[]) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[41];
-        for(int i = 0; i<n; i++) {
-            zerosum = 0;
-            onesum = 0;
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            finbonacci(k,arr);
-            bw.write(zerosum + " " + onesum+"\n");
+
+        arr.add(new Integer[]{1,0});
+        arr.add(new Integer[]{0,1});
+        for(int i = 2 ; i<=41;i++){
+            arr.add(new Integer[]{0,0});
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        int n = Integer.parseInt(br.readLine());
+        for(int i =0 ;i<n;i++){
+            int x = Integer.parseInt(br.readLine());
+            sb.append(Arrays.toString(finbonacci(x)).replace("[","").replace("]","").replace(",",""));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
     }
 }

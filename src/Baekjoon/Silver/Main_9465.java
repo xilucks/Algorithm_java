4aba59ac3291f3fc9fd3a18f8fc787ff7c27f7fd
xilcuks <siun0331@gmail.com>
2021-10-20 00:36:37 +0900

    BOJ/9465_스티커/실버2/다이나믹 프로그래밍

--- src/Baekjoon/Silver/Main_9465.java
+++ src/Baekjoon/Silver/Main_9465.java
@@ -1,1 +1,45 @@
-public class`
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_9465 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int C = Integer.parseInt(br.readLine());
+        int[][] arr, dp;
+        StringBuilder sb = new StringBuilder();
+        for(int c = 0; c<C; c++){
+            int N = Integer.parseInt(br.readLine());
+            arr = new int[2][N];
+            dp = new int[2][N];
+            int ans = 0;
+            for(int i = 0; i<2; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j = 0 ; j <N; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            for(int i = 0 ; i<N; i++){
+                if(i == 0){
+                    dp[0][i] = arr[0][i];
+                    dp[1][i] = arr[1][i];
+                }
+                else if(i==1){
+                    dp[0][i] = arr[0][i] + dp[1][i-1];
+                    dp[1][i] = arr[1][i] + dp[0][i-1];
+                }
+                else{
+                    dp[0][i] = arr[0][i] + Math.max(Math.max(dp[0][i-2],dp[1][i-2]),dp[1][i-1]);
+                    dp[1][i] = arr[1][i] + Math.max(Math.max(dp[1][i-2],dp[0][i-2]),dp[0][i-1]);
+                }
+
+                ans = Math.max(Math.max(dp[0][i],dp[1][i]),ans);
+            }
+            sb.append(ans+"\n");
+        }
+        System.out.println(sb);
+    }
+}

3e00d65fabb599988411b358f68d6565643d37cd
xilcuks <siun0331@gmail.com>
2021-10-23 01:54:15 +0900

    Programmers/내적/레벨1/월간 코드 챌린지

---
+++ src/programmers/level1/Solution_70128.java
@@ -0,0 +1,13 @@
+class Solution_70128 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i =0; i<a.length; i++){
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

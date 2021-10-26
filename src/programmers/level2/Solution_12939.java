082a418f0997208c4e639c0282456251398eddec
xilucks <siun0331@gmail.com>
2021-10-27 00:40:25 +0900

    programmers/최댓값과 최솟값/레벨2/연습문제

--- src/programmers/level2/Solution_12939.java
+++ src/programmers/level2/Solution_12939.java
@@ -1,2 +1,25 @@
-package programmers.level2;public class Solution_12939 {
+package programmers.level2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution_12939 {
+    public static String solution(String s) {
+        String answer = "";
+        String[] strs = s.split(" ");
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i<strs.length;i++){
+            arr.add(Integer.parseInt(strs[i]));
+        }
+        Collections.sort(arr);
+        answer += arr.get(0);
+        answer += " ";
+        answer += arr.get(arr.size()-1);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("1 2 3 4"));
+
+    }
 }

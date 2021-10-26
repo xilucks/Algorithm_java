afaf3fcd35b682d7cea3e6985496545710fde5df
xilucks <siun0331@gmail.com>
2021-10-27 00:40:58 +0900

    programmers/피보나치 수/레벨2/연습문제

--- src/programmers/level2/Solution_12973.java
+++ src/programmers/level2/Solution_12973.java
@@ -1,2 +1,27 @@
-package programmers.level2;public class Solution_12973 {
+package programmers.level2;
+
+import java.util.Stack;
+
+public class Solution_12973 {
+    public static int solution(String s)
+    {
+        int answer = -1;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(stack.isEmpty()) stack.push(ch);
+            else{
+                if(stack.peek() == ch) stack.pop();
+                else stack.push(ch);
+            }
+        }
+        if(stack.isEmpty()) answer = 1;
+        else answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("cdcd"));
+    }
 }

f05477cd68b6125eae80b15bf440311fa5c290f7
xilucks <siun0331@gmail.com>
2022-01-03 01:13:11 +0900

    인터페이스와 클래스의 상속

---
+++ src/study/InheritanceOfInterface_1.java
@@ -0,0 +1,20 @@
+package study;
+
+interface a1 {}
+interface b1 {}
+
+class C6 implements a1{
+
+}
+
+class D6 implements a1,b1{
+
+}
+class E6 extends C6 implements a1,b1{
+
+}
+public class InheritanceOfInterface_1 {
+    public static void main(String[] args) {
+
+    }
+}
